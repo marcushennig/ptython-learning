fb74416c54ad023de28e04bc32277e1f080b7bcb
marcushennig <35DEv5bq>
2017-11-12 20:41:28 +0100

    some playing around

--- day0.py
+++ day0.py
@@ -1,2 +1,13 @@
-x = "Hello World"
-print(x)
+# This is my first python program
+
+
+x = [1, 2, 3, 4, 5, 6]
+y={ 'x': 1,
+    'y': 2}
+
+x1 = [1, 2, 3, 4]
+x2 = ['a', 'b', 'c', 'd']
+x12 = dict(zip(x1, x2))
+
+print(x12)
+
