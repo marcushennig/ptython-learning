2505b95ce2b709977134fdd46979225258e0d678
Marcus Hennig <marcus.hennig@d-fine.de>
2017-11-14 13:52:31 +0100

    -learned about list, dictionaries and sets

--- day0.py
+++ day0.py
@@ -1,13 +1,33 @@
 # This is my first python program
 
 
-x = [1, 2, 3, 4, 5, 6]
-y={ 'x': 1,
-    'y': 2}
+def square(t: tuple):
+    _y = ()
+    for i in range(0, len(t)):
+        _y += (t[i] * t[i],)
+    return _y
 
-x1 = [1, 2, 3, 4]
-x2 = ['a', 'b', 'c', 'd']
-x12 = dict(zip(x1, x2))
 
-print(x12)
+def fibonacci(m: int, remainder='test'):
+    """Computes the nth Fibonacci number
+    n is an integer."""
+    result = []
+    a, b = 0, 1
+    print(remainder)
+    while a < m:
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+
+'''Build  dictionary '''
+lm = [j for j in range(0, 100)]
+print(lm)
+d = {(i, i): i ** 2 for i in range(0, 3)}
+if (2, 2) in d:
+    print(d[(2, 2)])
+print(d)
+
+for key in d.keys():
+    print(f'For {key} found value {d[key]}')
 
