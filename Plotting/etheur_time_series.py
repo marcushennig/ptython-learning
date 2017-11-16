71d4399b4097961ecd9ff1c97c3972a53a612ca9
Marcus Hennig <marcus.hennig@d-fine.de>
2017-11-16 15:07:45 +0100

    -play around with times

--- Plotting/etheur_time_series.py
+++ Plotting/etheur_time_series.py
@@ -15,7 +15,8 @@ kraken.close()
 # ----------------------------------------------------------------------------------------------------------------------
 
 T = np.array([k for k, v in history.items()])
-ETHEUR = np.array([v.open for k, v in history.items()])
+ETHEUR_low = np.array([v.low for k, v in history.items()])
+ETHEUR_high = np.array([v.high for k, v in history.items()])
 
 
 # Create a figure of size 8x6 inches, 80 dots per inch
@@ -28,7 +29,8 @@ plt.figure(figsize=(8, 6), dpi=80)
 # Create a new subplot from a grid of 1x1
 plt.subplot(1, 1, 1)
 
-plt.plot(T, ETHEUR, color='blue', linewidth=2.5, linestyle="-")
+plt.plot(T, ETHEUR_low, color=[0, 0, 1], linewidth=2.5, linestyle="-")
+plt.plot(T, ETHEUR_high, color=[1, 0, 0], linewidth=2.5, linestyle="-")
 
 plt.xlabel('time')
 plt.ylabel('ETH/EUR')
