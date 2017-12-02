5e5009909c4c89d3479ce97fc91987b5d37465db
marcushennig <35DEv5bq>
2017-12-02 21:01:59 +0100

    -added thx history

--- Plotting/etheur_time_series.py
+++ Plotting/etheur_time_series.py
@@ -1,13 +1,42 @@
 from Kraken.kraken_api import KrakenAPI
+from typing import Dict, Any
 
-import datetime
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+import datetime
+
 
 # ----------------------------------------------------------------------------------------------------------------------
+#
+# https://www.forbes.com/sites/wwoo/2017/09/29/is-bitcoin-in-a-bubble-check-the-nvt-ratio/#60c0324c6a23
+#
 # Load transaction history for ethereum
 # ----------------------------------------------------------------------------------------------------------------------
 
+def load_thx_file(path: str) -> Dict[Any, int]:
+
+    thx_time_series = {}
+    with open('ethereum-TxGrowth.csv') as tx_file:
+
+        reader = csv.reader(tx_file, delimiter=',')
+        # skip headers
+        next(reader, None)
+
+        for row in reader:
+
+            unixTimeStamp = int(row[1])
+            t = datetime.datetime.utcfromtimestamp(unixTimeStamp)
+            numberOfThx = int(row[2])
+
+            thx_time_series.update({t: numberOfThx})
+
+    return thx_time_series
+
+thx_time_series = load_thx_file('ethereum-TxGrowth.csv')
+
+thx_time = np.array([k for k, v in thx_time_series.items()])
+thx = np.array([v/1000 for k, v in thx_time_series.items()])
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -15,18 +44,17 @@ import matplotlib.pyplot as plt
 # ----------------------------------------------------------------------------------------------------------------------
 
 kraken = KrakenAPI()
-history = kraken.get_ohlc_data_for_asset('XETHZEUR', 240)
+history = kraken.get_ohlc_data_for_asset('XETHZEUR', 10080)
 kraken.close()
 
 # ----------------------------------------------------------------------------------------------------------------------
 # get data into numpy array
 # ----------------------------------------------------------------------------------------------------------------------
 
-time = np.array([datetime.datetime.utcfromtimestamp(t) for t, value in history.items()])
-
-ETHEUR_low = np.array([v.low for k, v in history.items()])
-ETHEUR_average = np.array([v.vwap for k, v in history.items()])
-ETHEUR_high = np.array([v.high for k, v in history.items()])
+t = np.array([datetime.datetime.utcfromtimestamp(t) for t, value in history.items()])
+p = np.array([v.vwap for k, v in history.items()])
+price_time = t[p > 0]
+price = p[p > 0]
 
 
 # Create a figure of size 8x6 inches, 80 dots per inch
@@ -40,9 +68,8 @@ plt.figure(figsize=(8, 6), dpi=80)
 plt.subplot(1, 1, 1)
 
 
-domain = ETHEUR_average > 0
-plt.plot(time[domain], ETHEUR_average[domain], color=[1, 0, 0], linewidth=2.5, linestyle="-")
-
+plt.plot(price_time, price, color=[1, 0, 0], linewidth=2.5, linestyle="-")
+plt.plot(thx_time, thx, color=[0, 0, 1], linewidth=2.5, linestyle="-")
 
 plt.xlabel('time')
 plt.ylabel('ETH/EUR')
